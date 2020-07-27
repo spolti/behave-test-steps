fefc7dfd27615ec39c782dc7006dfa89c1eb0416
spolti <fspolti@redhat.com>
2020-07-27 16:54:36 -0300

    test 6
    Signed-off-by: spolti <fspolti@redhat.com>

--- steps/podman_container.py
+++ steps/podman_container.py
@@ -94,7 +94,8 @@ class Container(object):
         self._create_container(**kwargs)
         self.logging.debug("Starting container '%s'..." % self.container.get('Id'))
         # d.start(container=self.container)
-        p.containers.get(self.container).start()
+        print(self.container)
+        p.containers.get(self.container.get('Id')).start()
         self.running = True
         self.ip_address = self.inspect()['NetworkSettings']['IPAddress']
 
